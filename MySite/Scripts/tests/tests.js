79e1d02e390efd70757235e495a1294b26b04773
james-wilcock <james.wilcock@climaxmedia.com>
2015-11-17 16:30:55 -0500

    Commit 1630

---
+++ MySite/Scripts/tests/tests.js
@@ -0,0 +1,8 @@
+﻿QUnit.test("hello test", function (assert) {
+    assert.ok(1 == "1", "Passed!");
+});
+QUnit.test("prettydate basics", function (assert) {
+    var now = "2008/01/28 22:25:00";
+    assert.equal(now, "2008/01/28 22:24:30");
+    
+});
